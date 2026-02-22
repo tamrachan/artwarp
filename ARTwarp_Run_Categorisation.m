2e4e76a7e7404f94566a10bfcbad0c32828aaa0c
KaiKerlaff <196723763+KaiKerlaff@users.noreply.github.com>
2026-02-22 21:46:39 +0000

    Update weight lengths to mean of inputs after each iteration

--- ARTwarp_Run_Categorisation.m
+++ ARTwarp_Run_Categorisation.m
@@ -268,6 +268,14 @@ for iterationNumber = 1:NET.maxNumIterations
     end
     % If no new categories were added, and no inputs were reclassified in the current iteration
     % then we've reached equilibrium. Thus, we can stop training.
+
+    % If updating weights according to warped contours:
+    if compareWarped == 1
+        % Resample each category's weight to be the mean lengths of the contours in
+        % that category
+        NET.weight = ARTwarp_Average_Weights(NET.weight, [DATA.length], [DATA.category]);
+    end
+
     if numChanges == 0
         break;
     end  
